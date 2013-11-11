d3a05e04a444149f01f9ebc4f44436a987769a6c
John Leacox <johnlcox@gmail.com>
2013-11-10 18:27:42 -0600

    Update to 2.2.1 and make specifying the version easy

--- attributes/deafult.rb
+++ attributes/deafult.rb
@@ -1,9 +1,14 @@
-default['play']['download_url']="http://downloads.typesafe.com/play/2.2.0/play-2.2.0.zip"
-default['play']['checksum']="b10eac7888d63e4b19566da9deb3b5f3babe3d251056d6cded669ba3faab0706"
-default['play']['version']="2.2.0"
+default['play']['version']="2.2.1"
+
+default['play']['download_url']="http://downloads.typesafe.com/play/#{node['play']['version']}/play-#{node['play']['version']}.zip"
+#default['play']['checksum']="b10eac7888d63e4b19566da9deb3b5f3babe3d251056d6cded669ba3faab0706"
+
+# default['play']['download_url']="http://downloads.typesafe.com/play/2.2.0/play-2.2.0.zip"
+# default['play']['checksum']="b10eac7888d63e4b19566da9deb3b5f3babe3d251056d6cded669ba3faab0706"
+# default['play']['version']="2.2.0"
 # TODO: Make installed version selectable using version attribute
 #default['play']['download_url']="http://downloads.typesafe.com/play/2.1.2/play-2.1.2.zip"
 #default['play']['checksum']="adebc13cecaaa0fb09dfadbda3191063590464633031ac80633db9f9a8c969db"
 default['play']['install_path']="/opt"
-default['play']['install_dir']="play-2.2.0"
+default['play']['install_dir']="play-#{node['play']['version']}"
 default['play']['symlink_dir']="play"
